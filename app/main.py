afb283c8d8b90e0cb992799b1226bb33786acd86
andr3if <andrei.florea003@gmail.com>
2025-11-11 17:37:31 -0800

    added fastapi-utilities

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI, Depends
+from fastapi_utilities import repeat_every
 
 from .dependencies import get_query_token
 from .routes import items
@@ -12,3 +13,8 @@ app.include_router(items.router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.on_event("startup")
+@repeat_every(seconds=3)
+async def print_hello():
+    print("hello world")
