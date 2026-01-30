935570a9b041cf4ce5526400f0922b5c8188f4dc
andr3if <andrei.florea003@gmail.com>
2026-01-29 17:08:46 -0800

    resolving copilot comments, adding additional tests

--- app/tests/test_core/test_scrapers/test_adzuna.py
+++ app/tests/test_core/test_scrapers/test_adzuna.py
@@ -139,7 +139,6 @@ async def test_parse_response_without_salary_max_gives_valid_job_list():
     salary_min = 50000
     description = "test_description"
     job1 = JobFactory(source=adzuna.source, salary=salary_min)
-    job2 = JobFactory(source=adzuna.source, salary=salary_min)
 
     input = {
         "results": [
@@ -152,24 +151,68 @@ async def test_parse_response_without_salary_max_gives_valid_job_list():
                 "salary_min": salary_min,
                 "location": {"display_name": job1.location},
             },
-            {
-                "title": job2.title,
-                "id": job2.source_id,
-                "company": {"display_name": job2.company_name},
-                "description": description,
-                "redirect_url": job2.url,
-                "salary_min": salary_min,
-                "location": {"display_name": job2.location},
-            },
         ]
     }
 
-    expected = [job1, job2]
+    expected = [job1]
     actual = await adzuna.parse_response(input)
 
     assert len(actual) == len(expected)
     assert actual[0].salary == expected[0].salary
-    assert actual[0].title != expected[1].title
+
+
+async def test_parse_response_without_salary_min_gives_valid_job_list():
+    adzuna = Adzuna()
+
+    salary_max = 50000
+    description = "test_description"
+    job1 = JobFactory(source=adzuna.source, salary=salary_max)
+
+    input = {
+        "results": [
+            {
+                "title": job1.title,
+                "id": job1.source_id,
+                "company": {"display_name": job1.company_name},
+                "description": description,
+                "redirect_url": job1.url,
+                "salary_max": salary_max,
+                "location": {"display_name": job1.location},
+            },
+        ]
+    }
+
+    expected = [job1]
+    actual = await adzuna.parse_response(input)
+
+    assert len(actual) == len(expected)
+    assert actual[0].salary == expected[0].salary
+
+
+async def test_parse_response_without_salary_gives_valid_job_list():
+    adzuna = Adzuna()
+
+    description = "test_description"
+    job1 = JobFactory(source=adzuna.source, salary=None)
+
+    input = {
+        "results": [
+            {
+                "title": job1.title,
+                "id": job1.source_id,
+                "company": {"display_name": job1.company_name},
+                "description": description,
+                "redirect_url": job1.url,
+                "location": {"display_name": job1.location},
+            },
+        ]
+    }
+
+    expected = [job1]
+    actual = await adzuna.parse_response(input)
+
+    assert len(actual) == len(expected)
+    assert actual[0].salary == expected[0].salary
 
 
 async def test_parse_response_with_empty_input_returns_empty_list():
