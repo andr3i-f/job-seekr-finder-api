c0d7e0f18f013d6a33d26af8c5c33490d7e658f1
andr3if <andrei.florea003@gmail.com>
2026-02-10 19:54:40 -0800

    wip

--- app/api/endpoints/resume.py
+++ app/api/endpoints/resume.py
@@ -11,21 +11,28 @@ client = AsyncGroq(
     api_key=get_settings().general.groq_api_key.render_as_string(hide_password=False)
 )
 
+def extract_text(file) -> str:
+    return ""
+
+async def parse_with_llm(text) -> str:
+
+    response = await client.chat.completions.create(
+        messages=[{"role": "user", "content": GROQ_PARSE_TEXT_PROMPT.format(text)}],
+        model="llama-3.1-8b-instant",
+        temperature=0
+    )
+        
+    return ""
+
 @router.get("/parse-resume")
 async def parse_resume(
     resume,
     _=Depends(get_current_user)
 ):
     # parse into a variable called text
-    text = ""
+    text = extract_text(resume)
+    parsed_text = parse_with_llm(text)
 
-
-    response = await client.chat.completions.create(
-        messages=[{"role": "user", "content": GROQ_PARSE_TEXT_PROMPT}],
-        model="llama-3.1-8b-instant",
-        temperature=0
-    )
-
-    return { "parsed": "hi" }
+    return { "parsed": parsed_text }
 
 
